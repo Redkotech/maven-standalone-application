77c2f4a90f31e6f749e2c145da3dfd93b11cf7ba
Redkotech <105688748+Redkotech@users.noreply.github.com>
2023-06-25 22:22:35 -0500

    Create MavenProjDSL.groovy

---
+++ MavenProjDSL.groovy
@@ -0,0 +1,51 @@
+job('mvn-project') {
+  displayName('my maven project')
+  description('Builds and deploys a Maven project')
+
+  scm {
+    git {
+      remote {
+        url('https://github.com/Redkotech/maven-standalone-application.git')
+        //credentials('github-credentials')
+      }
+      branch('master')
+    }
+  }
+
+  triggers {
+    scm('* * * * *')
+  }
+
+  steps {
+    maven {
+      goals('clean package')
+      //mavenInstallation('Maven 3.6.3')
+      rootPOM('pom.xml')
+      injectBuildVariables(true)
+    }
+  }
+
+  publishers {
+    archiveArtifacts('**/*jar')
+    archiveJunit('**/target/surefire-reports/TEST-*.xml')
+  }
+
+/*  postBuild {
+    always {
+      emailext {
+        recipientProviders {
+          developers()
+        }
+        subject('Build Notification - ${PROJECT_NAME} - Build #${BUILD_NUMBER} - ${BUILD_STATUS}')
+        body('''
+          <p>Build Information:</p>
+          <ul>
+            <li>Project: ${PROJECT_NAME}</li>
+            <li>Build Number: ${BUILD_NUMBER}</li>
+            <li>Status: ${BUILD_STATUS}</li>
+          </ul>
+        ''')
+      }
+    }
+  }*/
+}
